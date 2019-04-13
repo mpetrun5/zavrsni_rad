8c0d49e21637cc9f43033467c0f5838f208b7326
matija <matija.petrunic@fer.hr>
2019-04-14 00:10:38 +0200

    Update destination selector with title

--- scrapper/models.py
+++ scrapper/models.py
@@ -7,10 +7,12 @@ class Selector(models.Model):
     """
     Define css selectors and url needed for getting data.
     """
-    offers_url = models.CharField(max_length=100)
-    price = models.CharField(max_length=100)
-    destination = models.CharField(max_length=100)
     agency = models.OneToOneField(Agency, on_delete=models.CASCADE)
+    offers_url = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+
+    title = models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
     travel_plan = models.CharField(max_length=100, blank=True)
     price_includes = models.CharField(max_length=100, blank=True)
     price_excludes = models.CharField(max_length=100, blank=True)
