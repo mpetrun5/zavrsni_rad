fece1cd8b7ea8419c9a50342c1d23aa018d6d22e
matija <matija.petrunic@fer.hr>
2019-05-31 01:18:44 +0200

    Change price to decimal field

--- destination/models.py
+++ destination/models.py
@@ -11,12 +11,18 @@ class Destination(models.Model):
     agency = models.ForeignKey(Agency, on_delete=models.DO_NOTHING)
     original_url = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     num_of_nights = models.PositiveIntegerField()
     image = models.ImageField(upload_to='images/', blank=True)
 
     def __str__(self):
         return self.name
+
+    def agency_to_string(self):
+        """
+        Represent agency field as string for use with elasticsearch index.
+        """
+        return self.agency.name
 
 
 class Country(models.Model):
