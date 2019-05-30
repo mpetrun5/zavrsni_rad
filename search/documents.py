cec984c93df2ab1dc1e2b26fdf5ead5341d0d263
matija <matija.petrunic@fer.hr>
2019-05-31 01:19:50 +0200

    Add search integration with elasticsearch

---
+++ search/documents.py
@@ -0,0 +1,27 @@
+from django_elasticsearch_dsl import DocType, Index
+from django_elasticsearch_dsl import fields
+
+from destination.models import Destination
+
+destination = Index('destinations')
+destination.settings(
+    number_of_shards=1,
+    number_of_replicas=0
+)
+
+
+@destination.doc_type
+class DestinationDocument(DocType):
+    price = fields.FloatField(attr=None)
+    agency = fields.TextField(attr='agency_to_string')
+
+    class Meta:
+        model = Destination
+
+        fields = [
+            'id',
+            'image',
+            'name',
+            'description',
+            'num_of_nights',
+        ]
--- zavrsni_rad/urls.py
+++ zavrsni_rad/urls.py
@@ -18,6 +18,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from search.views import SearchView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('search/', SearchView.as_view, name='search')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
