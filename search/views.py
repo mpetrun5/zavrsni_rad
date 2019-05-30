4dd8cd73454b1837405ebe029e0137296835fc1f
matija <matija.petrunic@fer.hr>
2019-05-31 01:24:46 +0200

    Fix placing SearchView in wrong app

--- search/views.py
+++ search/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import View
+
+from search.documents import DestinationDocument
+
+
+class SearchView(View):
+
+    def get(self, request):
+        q = request.GET.get('q')
+
+        if q:
+            destinations = DestinationDocument.search().suggest(
+                'destinations',
+                q,
+                term={
+                    'field': 'name',
+                }
+            )
+
+        return json.dumps(destinations)
