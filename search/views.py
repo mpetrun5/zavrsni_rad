39dfb926f5c628e855ae70e6a00573c67b8b1de5
matija <matija.petrunic@fer.hr>
2019-06-03 03:10:40 +0200

    Add paginatino and price and nights slider

--- search/views.py
+++ search/views.py
@@ -1,3 +1,5 @@
+
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.views.generic import TemplateView
 from elasticsearch_dsl.query import MultiMatch
 
@@ -19,11 +21,56 @@ class SearchView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+        page = self.request.GET.get('page', 1)
         q = self.request.GET.get('q')
+
+        price = self.request.GET.get('price')
+        if price:
+            price_min, price_max = price.split(',')
+        else:
+            price_min = 0
+            price_max = 60000
+
+        num_of_nights = self.request.GET.get('num_of_nights')
+        if num_of_nights:
+            num_of_nights_min, num_of_nights_max = num_of_nights.split(',')
+        else:
+            num_of_nights_min = 0
+            num_of_nights_max = 30
+
+        form_data = {
+            'q': q
+        }
         if q:
-            query = MultiMatch(query=q, fields=['name'], fuzziness='AUTO')
-            context['destinations'] = DestinationDocument.search().query(
-                query
+            query = MultiMatch(query=q, fields=['name', 'description'], fuzziness='AUTO')
+            destinations = list(
+                DestinationDocument.search().query(
+                    query
+                )[0:1000]
             )
-        context['search_form'] = SearchForm()
+            if price:
+                destinations = [
+                    destination for destination in destinations
+                    if destination.num_of_nights <= int(num_of_nights_max)
+                    and destination.num_of_nights >= int(num_of_nights_min)
+                ]
+            if num_of_nights:
+                destinations = [
+                    destination for destination in destinations
+                    if destination.price <= int(price_max)
+                    and destination.price >= int(price_min)
+                ]
+
+            paginator = Paginator(destinations, 8)
+            try:
+                destinations = paginator.page(page)
+            except PageNotAnInteger:
+                destinations = paginator.page(1)
+            except EmptyPage:
+                destinations = paginator.page(paginator.num_pages)
+
+        context['search_form'] = SearchForm(data=form_data)
+        context['destinations'] = destinations
+
         return context
