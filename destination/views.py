efa171263ec8da27f10146ef59dfec5823b57446
matija <matija.petrunic@fer.hr>
2019-06-08 03:39:10 +0200

    Add reviews to destinations

--- destination/views.py
+++ destination/views.py
@@ -1,7 +1,15 @@
 from django.views.generic import DetailView
-from destination.models import Destination
+from destination.models import Destination, Review
 
 
 class DestinationDetailView(DetailView):
     queryset = Destination.objects.all()
     template_name = 'destination.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        destination = context['object']
+        context['reviews'] = Review.objects.filter(
+            destination=destination
+        ).order_by('-date_posted')
+        return context
